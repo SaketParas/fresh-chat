0b8f5c437f55e12e6ae910733e0888cdac566745
SaketParas <saketparas@gmail.com>
2020-11-09 15:49:06 +0530

    chat app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import FreshChat from 'react-freshchat'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  // ...
+  render() {
+    return <div>
+      <FreshChat
+        token='301abe17-bd46-49cc-af1f-5945434f3312'
+        email="user@email.com"
+        first_name="..."
+        onInit={widget => {
+          /* Use `widget` instead of `window.fcWidget`
+            widget.user.setProperties({
+              email: user.email,
+              first_name: user.firstName,
+              last_name: user.lastName,
+              phone: user.phoneNumber,
+            })
+          */
+        }}
+      />
     </div>
-  );
+  }
 }
-
 export default App;
---
+++ src/Components/Data.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Data(props) {
+    return (
+        <div className="">
+            <li class="list-group-item">{props.data} + hbhfb</li>
+        </div>
+    )
+}
+
+export default Data
